aca5b00a8737d72c705731c60f64e9635f5f1f2d
wsl <yewentai1216@gmail.com>
2025-02-17 22:58:05 +0100

    update

--- src/train.py
+++ src/train.py
@@ -4,13 +4,11 @@
 import os
 import pickle
 from datetime import date, datetime
-from typing import Dict, List
 
 import numpy as np
 import torch
 import torch.distributed as dist
 import torch.nn.functional as F
-from torch import Tensor
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, random_split
 from torch.utils.data.distributed import DistributedSampler
